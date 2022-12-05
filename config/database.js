b03870c3b35a3a226f362faa91ce2bea70f39740
Vaishnav Vijayan <vaishnavtdy@gmail.com>
2022-12-05 13:38:28 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -1,35 +1,20 @@
-// path: ./my-project/config/database.js
-
 module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+      },
+      options: {},
     },
-    useNullAsDefault: true,
   },
 });
-// module.exports = ({ env }) => ({
-//   defaultConnection: "default",
-//   connections: {
-//     default: {
-//       connector: "bookshelf",
-//       settings: {
-//         client: "mysql",
-//         host: env("DATABASE_HOST", "localhost"),
-//         port: env.int("DATABASE_PORT", 3306),
-//         database: env("DATABASE_NAME", "strapi"),
-//         username: env("DATABASE_USERNAME", "strapi"),
-//         password: env("DATABASE_PASSWORD", "strapi"),
-//       },
-//       options: {},
-//     },
-//   },
-// });
 
 // module.exports = ({ env }) => ({
 //   defaultConnection: "default",
@@ -75,5 +60,21 @@ module.exports = ({ env }) => ({
 //         ssl: true,
 //       },
 //     },
+//   },
+// });
+
+// path: ./my-project/config/database.js
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "strapi"),
+//       user: env("DATABASE_USERNAME", ""),
+//       password: env("DATABASE_PASSWORD", ""),
+//     },
+//     useNullAsDefault: true,
 //   },
 // });
